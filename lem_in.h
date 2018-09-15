83dabf48b9f3b09318f3a2736a01335d3acd3257
DavidMendelovits <dmendelo@e1z4r9p3.42.us.org>
2018-09-15 14:59:07 -0700

    I don't know what I'm doing

---
+++ lem_in.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lem_in.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmendelo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/15 14:30:46 by dmendelo          #+#    #+#             */
+/*   Updated: 2018/09/15 14:58:51 by dmendelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEM_IN_H
+# define LEM_IN_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+
+
+void			lem_in(void);
+
+#endif
